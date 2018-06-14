79666ae7561cbd7748243b906309b9372cd2a279
slzatz <slzatz@gmail.com>
2018-06-13 22:36:46 -0400

    m5sonos_menu5.py:now can play songs from the queue. Next task is to scroll beyond the visible screen and bring new rows into view

--- m5sonos_menu5.py
+++ m5sonos_menu5.py
@@ -28,15 +28,18 @@ with open('location', 'r') as f:
     loc = f.read()
 
 sub_topic = 'sonos/{}/track'.format(loc)
-pub_topic =  'sonos/{}'.format(loc)
+#pub_topic =  'sonos/{}'.format(loc)
 flag = bytearray(1)
 uri = 'http://192.168.1.126:5000/actions'
 
 print("mqtt_id =", mqtt_id)
 print("host =", mqtt_aws_host)
 print("subscribe topic =", sub_topic)
-print("publish topic =", pub_topic)
+print("uri = ", uri)
+#print("publish topic =", pub_topic)
 
+#each list corresponds to a page
+#page 4, the queue, gets filled in programmatically
 actions = [
 ['quieter', 'louder'],
 
@@ -71,12 +74,6 @@ actions = [
 []
 ]   
 
-
-
-# this becomes combo of row (which it is now) and page so 1:0 is play_pause and 2:0 is ...
-#page = 1 #0 = artist picture and track info; 1 = menu items, 2 = more menu items etc.
-# or page is sub-page
-
 tft = m5stack.Display()
 tft.font(tft.FONT_DejaVu18, fixedwidth=False)
 
@@ -89,15 +86,35 @@ def draw_menu(p):
     page = p
     row = _N
     tft.clear()
-    #for i,action in new_actions[p].items():
     for i,action in enumerate(actions[p]):
         tft.text(20, _N+i*25, action)
 
     tft.text(5, row, ">")
 
-def display_queue(q):
+def display_image():
+  tft.clear()
+  artist = track_info.get('artist', '')
+  if artist:
+    try:
+      tft.image(0,0,'/sd/{}.jpg'.format(artist.lower().replace(' ', '_')))
+    except:
+      pass
+  tft.text(5, 5, artist+"\n") 
+
+  title = wrap(track_info.get('title', ''), 28) # 28 seems right for DejaVu18
+  for line in title:
+    tft.text(5, tft.LASTY, line+"\n")
+
+def display_queue():
     global row
     global page
+    try:
+        response = urequests.post(uri, json={'action':'list_queue'})
+        q = response.json()
+    except Exception as e:
+        q = ["There was a problem"]
+        print("error =", e)
+    response.close()
     page = 4
     row = _N
     tft.clear()
@@ -105,6 +122,7 @@ def display_queue(q):
     for i,track in enumerate(q):
         tft.text(20, _N+i*25, track)
     tft.text(5, row, '>')
+    actions[4] = q 
 
 def wrap(text,lim):
     lines = []
@@ -135,38 +153,21 @@ def subscb(task):
 def pubcb(pub):
     print("[{}] Published: {}".format(pub[0], pub[1]))
 
-def display_image():
-
-  tft.clear()
-  artist = track_info.get('artist', '')
-  if artist:
-    try:
-      tft.image(0,0,'/sd/{}.jpg'.format(artist.lower().replace(' ', '_')))
-    except:
-      pass
-  tft.text(5, 5, artist+"\n") 
-
-  title = wrap(track_info.get('title', ''), 28) # 28 seems right for DejaVu18
-  for line in title:
-    tft.text(5, tft.LASTY, line+"\n")
-
 def datacb(msg):
-  global page
-  global row
-  #global track_info
-  #track_info = msg
-  page = 0
-  row = _N
-  print("[{}] Data arrived - topic: {}, message:{}".format(msg[0], msg[1], msg[2]))
+    global page
+    global row
+    page = 0
+    row = _N
+    print("[{}] Data arrived - topic: {}, message:{}".format(msg[0], msg[1], msg[2]))
 
-  try:
-    z = json.loads(msg[2])
-  except Exception as e:
-    print(e)
-    z = {}
+    try:
+        z = json.loads(msg[2])
+    except Exception as e:
+        print(e)
+        z = {}
 
-  track_info.update(z)
-  display_image()
+    track_info.update(z)
+    display_image()
 
 # note published callback doesn't seem to do anything
 mqttc = network.mqtt(mqtt_id, mqtt_aws_host, connected_cb=conncb,
@@ -180,50 +181,49 @@ mqttc.subscribe(sub_topic)
 
 # ButtonA
 def button_hander_a(pin, pressed):
-  global flag
-  global row
-  if pressed:
-    print("A pressed")
-    if page:
-      tft.text(5, row, "  ")
-      row+=25
-      tft.text(5, row, ">")
-    else:
-      #if image showing -> quieter
-      row = _N 
-      flag = 1
+    global flag
+    global row
+    if pressed:
+        print("A pressed")
+        if page:
+            tft.text(5, row, "  ")
+            row+=25
+            tft.text(5, row, ">")
+        else:
+            #if image showing -> quieter
+            row = _N 
+            flag = 1
 
-    m5stack.tone(1800, duration=10, volume=1)
+        m5stack.tone(1800, duration=10, volume=1)
 
 # ButtonB
 def button_hander_b(pin, pressed):
-  global flag
-  if pressed:
-    print("B pressed")
-    if page:
-      flag = 1
-    else:
-      draw_menu(1)  
+    global flag
+    if pressed:
+        print("B pressed")
+        if page:
+            flag = 1
+        else:
+            draw_menu(1)  
 
-    m5stack.tone(1800, duration=10, volume=1)
+        m5stack.tone(1800, duration=10, volume=1)
 
 # ButtonC
 def button_hander_c(pin, pressed):
-  global flag
-  global row
-  if pressed:
-    print("C pressed")
-    if page:
-      tft.text(5, row, "  ")
-      row-=25
-      tft.text(5, row, ">")
-      m5stack.tone(1800, duration=10, volume=1)
-    else:
-      #if image showing -> louder
-      row = _N + 25
-      flag = 1
+    global flag
+    global row
+    if pressed:
+        print("C pressed")
+        if page:
+            tft.text(5, row, "  ")
+            row-=25
+            tft.text(5, row, ">")
+        else:
+            #if image showing -> louder
+            row = _N + 25
+            flag = 1
 
-    m5stack.tone(1800, duration=10, volume=1)
+        m5stack.tone(1800, duration=10, volume=1)
 
 a = m5stack.ButtonA(callback=button_hander_a)
 b = m5stack.ButtonB(callback=button_hander_b)
@@ -233,35 +233,38 @@ draw_menu(1)
 
 cur_time = 0
 while 1:
-  t = time()
-  if t > cur_time + 600:
-    print(strftime("%c", localtime()))
-    cur_time = t
-  #gc.collect()
-  #if page == 1: xxxx
-  #if flag == 
-  if flag:
-    action_num = (row-_N)//25
-    print("action number =", action_num)
-    action = actions[page][action_num]
-    print("action =", action)
-    if 'menu' in action:
-      draw_menu(int(action[-1]))
-      flag = 0
-      continue
-    elif action == 'queue':
-      response = urequests.post(uri, json={'action':'list_queue'})
-      queue = response.json()
-      actions[4] = queue
-      display_queue(queue)
-      flag = 0
-      continue
-    try:
-      mqttc.publish(pub_topic, json.dumps({'action':action}))
-    except Exception as e:
-      print(e)
-    flag = 0
-    if page: # page could equal zero if this was volume on image
-      page = 0
-      display_image()
-  sleep(.1)
+    t = time()
+    if t > cur_time + 600:
+        print(strftime("%c", localtime()))
+        cur_time = t
+    if flag:
+        action_num = (row-_N)//25
+        print("action number =", action_num)
+
+        if page < 4:
+            action = actions[page][action_num]
+            print("action =", action)
+            if 'menu' in action:
+                draw_menu(int(action[-1]))
+                flag = 0
+                continue
+            if 'queue' in action:
+                display_queue()
+                flag = 0
+                continue
+        else:
+            action = "play_queue "+str(action_num)
+            print("action =", action)
+
+        try:
+            response = urequests.post(uri, json={'action':action})
+            response.close()
+        except Exception as e:
+            print("error =", e)
+
+        flag = 0
+        if page: # page could equal zero if this was volume on image
+            page = 0
+            display_image()
+    #gc.collect()
+    sleep(.1)
